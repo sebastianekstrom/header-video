ed2c146b56cadd967e16709fbac3568df8617bb0
sekstrom <sebastian.ekstrom@creuna.se>
2015-04-13 10:59:32 +0200

    Cleanup of JS code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var HeaderVideo = (function ($, document) {
-    
+
     var settings = {
         container: $('.header-video'),
         video: '#video',
@@ -8,13 +8,13 @@ var HeaderVideo = (function ($, document) {
         videoCloseTrigger: $('#video-close-trigger'),
         teaserVideo: $('#teaser-video'),
         autoPlayVideo: false
-    }
+    };
 
     var init = function(options){
         settings = $.extend(settings, options);
         getVideoDetails();
         setFluidContainer();
-        bindClickAction();
+        bindClickActions();
         settings.videoCloseTrigger.hide();
         
         if(videoDetails.teaser) {
@@ -24,42 +24,9 @@ var HeaderVideo = (function ($, document) {
         if(settings.autoPlayVideo) {
             appendFrame();
         }
-    }
-
-    var getVideoDetails = function() {
-        videoDetails = {
-            id: settings.header.attr('data-video-src'),
-            teaser: settings.header.attr('data-teaser-source'),
-            provider: settings.header.attr('data-provider').toLowerCase(),
-            videoHeight: settings.header.attr('data-video-height'),
-            videoWidth: settings.header.attr('data-video-width')
-        }
-        return videoDetails;
     };
 
-    var setFluidContainer = function () {
-
-        settings.container.data('aspectRatio', videoDetails.videoHeight / videoDetails.videoWidth);
-
-        $(window).resize(function(){
-            var winWidth = $(window).width(),
-                winHeight = $(window).height();
-
-            settings.container
-                .width(Math.ceil(winWidth))
-                .height(Math.ceil(winWidth * settings.container.data('aspectRatio')));
-
-            if(winHeight < settings.container.height()) {
-                settings.container
-                    .width(Math.ceil(winWidth))
-                    .height(Math.ceil(winHeight));
-            }
-
-        }).trigger('resize');
-
-    };
-
-    var bindClickAction = function() {
+    var bindClickActions = function() {
         settings.videoTrigger.on("click", function(e) {
             e.preventDefault();
             appendFrame();
@@ -69,6 +36,39 @@ var HeaderVideo = (function ($, document) {
             e.preventDefault();
             removeFrame();
         });
+    };
+
+    var getVideoDetails = function() {
+        // Get all the data attributes from the HTML container and return them as an object for easy data retrieval
+        videoDetails = {
+            id: settings.header.attr('data-video-src'),
+            teaser: settings.header.attr('data-teaser-source'),
+            provider: settings.header.attr('data-provider').toLowerCase(),
+            videoHeight: settings.header.attr('data-video-height'),
+            videoWidth: settings.header.attr('data-video-width')
+        };
+        return videoDetails;
+    };
+
+    var setFluidContainer = function () {
+        settings.container.data('aspectRatio', videoDetails.videoHeight / videoDetails.videoWidth);
+
+        $(window).resize(function(){
+            var winWidth = $(window).width(),
+                winHeight = $(window).height();
+
+            settings.container
+                .width(Math.ceil(winWidth)) // Round up to the nearest pixel value to prevent breaking of layout
+                .height(Math.ceil(winWidth * settings.container.data('aspectRatio'))); // Set the videos aspect ratio, see https://css-tricks.com/fluid-width-youtube-videos/
+
+            if(winHeight < settings.container.height()) {
+                settings.container
+                    .width(Math.ceil(winWidth))
+                    .height(Math.ceil(winHeight));
+            }
+
+        }).trigger('resize'); // Trigger resize to force it to run on page load as well
+
     };
 
     var appendTeaserVideo = function() {
@@ -124,13 +124,9 @@ var HeaderVideo = (function ($, document) {
     };
 
     var isMobile = function () {
-        if($(window).width() < 900 && Modernizr.touch) {
-            return true;
-        }
-        else {
-            return false;
-        }
-
+        // A super basic way of detecting mobile devices. Should be extended to a more
+        // fool proof way in a production enviroment.
+        return Modernizr.touch;
     }
 
     return {
